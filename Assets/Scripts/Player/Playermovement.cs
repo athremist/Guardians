ddd95ff0cc65e232031498cf4c5ab275b69cb74b
athremist <andrew2255@hotmail.com>
2018-04-27 20:00:37 -0400

    ALOT OF STUFF including tiles
    Added all required sprites, made beta town1 with tilemap, made tile
    class and tilemanager to extract data from tilemap into data in code for
    tiles. Just need to add everything else for interacting and actually
    moving on tiles. Made class for pickups, player.

---
+++ Assets/Scripts/Player/Playermovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+    Vector2 StartPostition;
+    Vector2 EndPostition;
+    Vector2 Dirction;
+
+    void Start()
+    {
+        StartPostition = gameObject.transform.position;
+        EndPostition = Vector2.zero;
+
+    }
+
+    void Update()
+    {
+        //TODO: If input tile.nextTile
+        //TODO: implement turning AS WELL AS moving. Maybe?(Turning will set nextTile and moving will move to it.)
+    }
+
+
+}
---
+++ Assets/Scripts/Tile/Tile.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile
+{
+    Vector2 Coords; //Position of tile on the map
+    TileType Type; //What kind of tile
+    //Interactable InteractableObject;
+    //Pickup Pickup; //What pickup is on the tile/non
+    bool Walkable; //Is the tile walkable?
+
+    public Tile()
+    {
+
+    }
+
+    public Tile(Vector2 aCoodinates, TileType aTileType)//, Pickup aPickup)
+    {
+        Coords = aCoodinates;
+        Type = aTileType;
+    }
+
+    public Vector2 GetTileCoordinates()
+    {
+        return Coords;
+    }
+
+    public TileType GetTileType()
+    {
+        return Type;
+    }
+
+    //public Interactable Object()
+    //{
+    //    return interactableObject;
+    //}
+
+    public bool IsWalkable()
+    {
+        return Walkable;
+    }
+
+    //Tile manager will set tile to walkable depending on the tiletype
+    protected void SetWalkable(bool aIsWalkable)
+    {
+        Walkable = aIsWalkable;
+    }
+
+    public enum TileType
+    {
+        Ground,
+        Wall,
+        Ledge,
+        Grass,
+        Cave,
+        Water,
+        Door,
+        Stairs
+    }
+}
